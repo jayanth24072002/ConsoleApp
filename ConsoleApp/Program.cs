77eb315054f37d9e6b693d9d61d89fa70c3a174f
Jayanth A <ajayanth897@gmail.com>
2024-09-02 15:05:13 +0530

    Now we have many messages

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello,C# Academy,modified the changes"); Console.WriteLine("Hello,C# Academy,modified the changes");
 Console.WriteLine("Hello,C# Academy,modified the changes");
